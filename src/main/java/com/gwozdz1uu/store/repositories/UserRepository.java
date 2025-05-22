598ee0d813389b9e5b7fc8ebd9e6b6eb66117b50
Gwozdz1uu <gwozdeckidaniel@gmail.com>
2025-05-22 23:17:41 +0200

    UserDto, responseEntity

--- src/main/java/com/gwozdz1uu/store/repositories/UserRepository.java
+++ src/main/java/com/gwozdz1uu/store/repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package com.gwozdz1uu.store.repositories;
 
 import com.gwozdz1uu.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
