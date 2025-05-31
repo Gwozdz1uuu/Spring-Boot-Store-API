7ee909e67548c2e189c8a5585aba55e5b5fdcc4b
Gwozdz1uu <gwozdeckidaniel@gmail.com>
2025-05-31 13:05:14 +0200

    Creaating cart, cartMapper fix

--- src/main/java/com/gwozdz1uu/store/mappers/CartMapper.java
+++ src/main/java/com/gwozdz1uu/store/mappers/CartMapper.java
@@ -1,12 +1,46 @@
 package com.gwozdz1uu.store.mappers;
 
 import com.gwozdz1uu.store.dtos.CartDto;
+import com.gwozdz1uu.store.dtos.CartItemDto;
+import com.gwozdz1uu.store.dtos.CartProductDto;
 import com.gwozdz1uu.store.entities.Cart;
+import com.gwozdz1uu.store.entities.CartItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+import java.math.BigDecimal;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
 public interface CartMapper {
     @Mapping(source="id", target = "id")
     CartDto toDto(Cart cart);
+
+//    CartItemDto toDto(CartItem cartItem);
+
+
+    default CartItemDto toDto(CartItem cartItem) {
+        if (cartItem == null) {
+            return null;
+        }
+
+        CartItemDto cartItemDto = new CartItemDto();
+
+        var price = cartItem.getProduct().getPrice();
+        var quantity = cartItem.getQuantity();
+
+        // Map nested product manually
+        if (cartItem.getProduct() != null) {
+            CartProductDto productDto = new CartProductDto();
+            productDto.setId(cartItem.getProduct().getId());
+            productDto.setName(cartItem.getProduct().getName());
+            productDto.setPrice(price);
+            cartItemDto.setProduct(productDto);
+        }
+
+        cartItemDto.setQuantity(quantity);
+        cartItemDto.setTotalPrice(cartItem.getTotalPrice());
+
+        return cartItemDto;
+    }
+
 }
